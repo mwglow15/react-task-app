4796b1ad89e51f02c2884c702f23c914d638611c
Matt Glowacki <mattwglowacki@gmail.com>
2023-07-16 10:08:24 -0400

    Complete Simple to-do app with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
+import Overview from './components/Overview'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  state = { 
+    nextID: 2,
+    tasks: [{name: 'test', ID: 0, editView: false}, {name: 'test2', ID: 1, editView: false}]
+  }
+
+  addNewTask = (e) => {
+    e.preventDefault()
+    const newTask = e.target[0].value
+    if(newTask) {
+      const newTasks = [...this.state.tasks, {name: newTask, ID: this.state.nextID, editView: false}]
+      this.setState({nextID: this.state.nextID + 1, tasks: newTasks })
+    }
+  }
+
+  deleteTask = (e) => {
+    const taskID = Number(e.target.value)
+    
+    const indexToDelete = this.state.tasks.findIndex(task => {
+      return task.ID === taskID
+    })
+
+    const newState = [
+      ...this.state.tasks.slice(0, indexToDelete),
+      ...this.state.tasks.slice(indexToDelete + 1)
+    ]
+
+    this.setState({tasks: newState})
+  }
+
+  editTask = (e) => {
+    const taskID = Number(e.target.value)
+
+    const indexToEdit = this.state.tasks.findIndex(task => {
+      return task.ID === taskID
+    })
+    
+    const taskEditView = this.state.tasks[indexToEdit]
+    taskEditView.editView = true
+
+    const newState = this.state.tasks.toSpliced(indexToEdit, 1, taskEditView)
+
+    this.setState({tasks: newState})
+  }
+
+  confirmEdit = (e) => {
+    e.preventDefault()
+    const taskID = Number(e.target.attributes.value.value)
+    const indexToEdit = this.state.tasks.findIndex(task => {
+      return task.ID === taskID
+    })
+
+    const newTaskName = document.getElementById('newTaskName').value
+
+    const editedTask = this.state.tasks[indexToEdit]
+
+    editedTask.name = newTaskName
+    editedTask.editView = false
+
+    const newState = this.state.tasks.toSpliced(indexToEdit, 1, editedTask)
+
+    this.setState({tasks: newState})
+  }
+  
+
+  render() {
+    return (
+      <div>
+        <InputForm addNewTask = {this.addNewTask} />
+        <Overview 
+          tasks = {this.state.tasks} 
+          deleteTask = {this.deleteTask} 
+          editTask = {this.editTask} 
+          confirmEdit = {this.confirmEdit}
+        />
+      </div>
+    )
+  }
 }
 
-export default App;
+class InputForm extends Component{
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    const {addNewTask} = this.props
+    return(
+      <form onSubmit={addNewTask}>
+        <input type="text" placeholder='New Task...' />
+        <button type='submit'>Submit</button>
+        <button type='bigButton' id="bigButt-ons">Bebe's Big Button</button>
+      </form>
+    )
+  }
+}
---
+++ src/components/Overview.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react'
+
+export default class Overview extends Component{
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    const editing = this.props.tasks.some(task => {
+      return task.editView === true
+    })
+    const taskList = this.props.tasks.map(task => {
+      if (task.editView) {
+        return(
+          <li key={task.ID}>
+            <form onSubmit={this.props.confirmEdit} value={task.ID}>
+              <input placeholder={task.name} id='newTaskName'/>
+              <button value={task.Id} type='submit'>Confirm Edit</button>
+            </form>
+          </li>
+        )
+      } else {
+        return (
+          <li key={task.ID}>
+            { task.name }  
+            <button value={task.ID} onClick={this.props.editTask} disabled={editing}>Edit</button>
+            <button value={task.ID} onClick={this.props.deleteTask} disabled={editing}>Delete</button>
+          </li> 
+        )
+      }
+    })
+
+    return(
+      <ul className="taskCard">
+        {taskList}
+      </ul>
+    )
+  }
+}
